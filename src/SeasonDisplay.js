68470e395e8d02a2bfef16deb80ec173e63ab0f3
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-15 08:44:01 +0100

    [1] Initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+/*jshint esversion:6*/
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return (
+    <div>Season display</div>
+  );
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+/*jshint esversion:6 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  // when you define the constructor() function inside your
+  // component you are essentially overriding the constructor()
+  // function that is inside of React.Component class
+  // this is why you have to call super(props);
+  constructor(props) {
+    super(props);
+// initialize the state object with a 'lat' property and set it to null
+// (will eventually be a number)
+// THIS IS THE ONLY TIME we do direct assingment to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+  }
+
+  render() {
+    return (
+    <div>Latitude: {this.state.lat}</div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
