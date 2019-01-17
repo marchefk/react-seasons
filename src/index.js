893c2e6846c1616bc5e2d4b93c56fcc2c070536b
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-17 21:52:04 +0100

    [2] SeasonDisplay refactor + added Spinner + css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /*jshint esversion:6 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   // when you define the constructor() function inside your
@@ -12,20 +14,29 @@ class App extends React.Component {
 // initialize the state object with a 'lat' property and set it to null
 // (will eventually be a number)
 // THIS IS THE ONLY TIME we do direct assingment to this.state
-    this.state = { lat: null };
+    this.state = {
+      lat: null,
+      errorMessege: ''
+    };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => console.log(err)
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return (
-    <div>Latitude: {this.state.lat}</div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner />;
   }
 }
 
